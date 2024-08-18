6886e9cdb22e0b4fe7594eb6bcde39923c32c7e6
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-18 12:09:24 +0200

    post through ajax from modal

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse
 from .models import Post
 from .forms import CreatePostForm
 
@@ -12,7 +14,23 @@ def index(request):
 
 
 def create_post(request):
-    create_post_form = CreatePostForm()
-    return render(request, 'posts/create_post_form.html', {
-        "create_post_form": create_post_form,
-    })
+    # if request.method == "POST":
+    #     collaborate_form = CollaborateRequestForm(data=request.POST)
+    #     if collaborate_form.is_valid():
+    #         collaborate_form.save()
+    #         messages.add_message(
+    #             request, messages.SUCCESS,
+    #             'Collaboration request received! I endeavour to respond within 2 working days.'
+    #         )
+    if request.method == "POST":
+        create_post_form = CreatePostForm(data=request.POST)
+        if create_post_form.is_valid():
+            new_post = create_post_form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return HttpResponse(status=200)
+    else:
+        create_post_form = CreatePostForm()
+        return render(request, 'posts/create_post_form.html', {
+            "create_post_form": create_post_form,
+        })
--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,12 @@ const createPostBtn = document.getElementById('create_post_btn');
 $('#create_post_btn').on('click', () => {
   $('#general_modal .modal-title').text('Create Post');
   $('#general_modal button[data-bs-dismiss]').text('Cancel');
-  $('#general_modal .btn-primary').text('Post');
+  $('#general_modal .btn-primary')
+    .text('Post')
+    .on('click', () => {
+      $('#general_modal').modal('hide');
+      $.post('create-post', $('#create_post_form').serialize());
+    });
   $.get('create-post', (data, status) => {
     $('#general_modal .modal-body').html(data);
     // @TODO: Handle errors
