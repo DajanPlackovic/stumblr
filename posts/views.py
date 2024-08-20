a85ae69dd11d90575d6672bc8cf6e8a3e2180e50
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-20 19:06:11 +0200

    add docstrings to all views

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,13 @@ from .forms import CreatePostForm
 # Create your views here.
 # Post Views
 def index(request):
+    """
+    Renders the index page, displaying all posts.
+
+    :param request: (HttpRequest): The HTTP request object.
+
+    :return: HttpResponse: The rendered index.html template with the post_list context variable.
+    """
     post_list = Post.objects.all().order_by('-time_posted')
     return render(request, 'posts/index.html', {
         "post_list": post_list
@@ -16,6 +23,18 @@ def index(request):
 
 def create_post(request):
     # @TODO: add error handling
+    """
+    Creates a new post.
+
+    If the request is a POST, validates the input and creates a new Post
+    with the given text and author. If the request is a GET, renders a
+    form to create a post.
+
+    :param request: (HttpRequest): The HTTP request object.
+    :return: A JSON response with the status 200 if the post is created
+        successfully, or a HTML page with the form to create a post if the
+        request is a GET request.
+    """
     if request.method == "POST":
         create_post_form = CreatePostForm(data=request.POST)
         if create_post_form.is_valid():
@@ -33,6 +52,14 @@ def create_post(request):
 
 
 def delete_post(request, post_id):
+    """
+    Deletes a post for the current user.
+
+    :param request: A POST request or a GET request.
+    :param post_id: The id of the post to delete.
+    :return: A JSON response with the status 200 if the post is deleted correctly, or a HTML page with the post
+             to be deleted if the request is a GET request.
+    """
     post = get_object_or_404(Post, pk=post_id)
 
     if post.author == request.user:
@@ -47,6 +74,12 @@ def delete_post(request, post_id):
 
 # Collection Views
 def collection_list(request):
+    """
+    Renders a page with a list of all collections.
+
+    :param request: The HTTP request object.
+    :return: A rendered HTML page with the list of collections.
+    """
     collections = Collection.objects.all()
     return render(request, 'posts/collections.html', {
         "collections": collections
@@ -54,6 +87,13 @@ def collection_list(request):
 
 
 def individual_collection(request, slug):
+    """
+    Renders a single collection and all its posts.
+
+    :param request: The HTTP request object.
+    :param slug: The slug of the collection to be rendered.
+    :return: A rendered HTML page with the collection and its posts.
+    """
     collection = get_object_or_404(Collection, slug=slug)
     posts = collection.posts.all()
     return render(request, 'posts/single_collection.html', {
@@ -63,6 +103,23 @@ def individual_collection(request, slug):
 
 
 def collection_menu(request, post_id):
+    """
+    Adds or removes a post from the user's collections.
+
+    The POST request must contain a parameter 'collection' with a list of collection IDs.
+    The method will remove the post from all collections that the user is a part of,
+    and add the post to the collections in the list.
+
+    If the request is a GET request, return a JSON response with a list of dictionaries,
+    each dictionary containing the name and id of a collection, and a boolean value
+    indicating if the post is already in the collection. This is then used by JS
+    on the client side to generate a checkbox menu with the available collections.
+
+    :param request: A POST request with a 'collection' parameter, or a GET request.
+    :param post_id: The id of the post to add or remove from collections.
+    :return: For GET: A JsonResponse with the list of available collections.
+    For POST: An HttpResponse with the status 200 after the post is added.
+    """
     if request.method == 'POST':
         post = get_object_or_404(Post, pk=post_id)
         for collection in request.user.collections.all():
@@ -82,6 +139,13 @@ def collection_menu(request, post_id):
 
 
 def create_collection(request):
+    """
+    Creates a new collection for the current user.
+
+    :param request: A POST request with a 'name' parameter for the new collection.
+    :return: A JSON response with the name and id of the new collection.
+    :raises: HttpResponse or JsonResponse with status 500 if there is an error, or if the collection already exists.
+    """
     name = dict(request.POST)["name"][0]
     try:
         collection = request.user.collections.create(name=name)
@@ -95,6 +159,14 @@ def create_collection(request):
 
 
 def delete_collection(request, collection_id):
+    """
+    Deletes a collection for the current user.
+
+    :param request: A POST request or a GET request.
+    :param collection_id: The id of the collection to delete.
+    :return: A JSON response with the status 200 if the collection is deleted correctly, or a HTML page with the collection
+             to be deleted if the request is a GET request.
+    """
     collection = get_object_or_404(Collection, pk=collection_id)
 
     if collection.author == request.user:
@@ -109,6 +181,13 @@ def delete_collection(request, collection_id):
 
 # User Views
 def user(request, user_id):
+    """
+    Shows a user's profile page with their posts and collections.
+
+    :param request: The request object.
+    :param user_id: The id of the user whose profile is shown.
+    :return: A rendered HTML page with the user's data.
+    """
     displayed_user = User.objects.filter(pk=user_id).first()
     posts = displayed_user.posts.all().order_by('-time_posted')
     collections = displayed_user.collections.all()
