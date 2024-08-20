828f7804b1dbd86e15c827e54d5ccf8ea730351b
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-20 20:55:58 +0200

    refactor main.js

--- static/js/main.js
+++ static/js/main.js
@@ -23,8 +23,11 @@ function updateModal(title, cancelBtn, postBtn) {
 /*=============================================
 =                Error Handling               =
 =============================================*/
-function showErrorOrInfo(text, error) {
-  if (!text) {
+function showErrorOrInfo(response, error) {
+  let text;
+  if (response.responseJSON?.text) {
+    text = response.responseJSON.text;
+  } else {
     text = 'An error has occurred';
     error = true;
   }
@@ -56,6 +59,29 @@ const showError = (text) => showErrorOrInfo(text, true);
 const showInfo = (text) => showErrorOrInfo(text, false);
 
 /*=============================================
+=                Ajax Wrappers                =
+=============================================*/
+
+function ajaxWrapper(type, rest = {}) {
+  $.ajax({
+    type: type,
+    error: (data) => {
+      showError(data);
+    },
+    ...rest,
+  });
+}
+
+const ajaxGet = (rest = {}) => ajaxWrapper('GET', rest);
+const ajaxPost = (rest = {}) =>
+  ajaxWrapper('POST', {
+    ...rest,
+    headers: {
+      'X-CSRFToken': csrftoken,
+    },
+    mode: 'same-origin',
+  });
+/*=============================================
 =                 Action Menu                 =
 =============================================*/
 
@@ -64,34 +90,21 @@ $('#create_post_btn').on('click', () => {
   const url = '/create-post';
   const form = '#create_post_form';
   $('#general_modal .btn-primary').on('click', () => {
-    $.ajax({
-      type: 'POST',
-      url: url,
-      data: $(form).serialize(),
-      success: (data) => {
-        $('#general_modal').modal('hide');
-        // @TODO: if user is already on posts page, insert post instead of reloading
-        window.location.replace('');
-      },
-      error: (data) => {
-        showError(data.text);
-      },
-    });
+    const data = $(form).serialize();
+    const success = () => {
+      $('#general_modal').modal('hide');
+      // @TODO: if user is already on posts page, insert post instead of reloading
+      window.location.replace('');
+    };
+    ajaxPost({ url, data, success });
   });
-  $.ajax({
-    type: 'GET',
-    url: url,
-    data: $(form).serialize(),
-    beforeSend: () => {
-      $('#general_modal .modal-body').html(loader);
-    },
-    success: (data) => {
-      $('#general_modal .modal-body').html(data);
-    },
-    error: (data) => {
-      showError(data.text);
-    },
-  });
+  const beforeSend = () => {
+    $('#general_modal .modal-body').html(loader);
+  };
+  const success = (data) => {
+    $('#general_modal .modal-body').html(data);
+  };
+  ajaxGet({ url, beforeSend, success });
 });
 
 /*=============================================
@@ -105,47 +118,29 @@ $('button[data-action="delete"]').on('click', function (e) {
   $('#general_modal .btn-primary')
     .addClass('btn-danger')
     .on('click', () => {
-      $('#general_modal').modal('hide');
-      $.ajax({
-        type: 'POST',
-        url: url,
-        data: $('#delete_post_form').serialize(),
-        success: () => {
-          // @TODO: just remove the deleted post, instead of reloading
-          window.location.replace('');
-        },
-        error: (data) => {
-          showError(data.text);
-        },
-      });
+      const data = $('#delete_post_form').serialize();
+      const success = () => {
+        $('#general_modal').modal('hide');
+        // @TODO: just remove the deleted post, instead of reloading
+        window.location.replace('');
+      };
+      ajaxPost({ url, data, success });
     });
-  $.ajax({
-    type: 'GET',
-    url: url,
-    data: $('#delete_post_form').serialize(),
-    beforeSend: () => {
-      $('#general_modal .modal-body').html(loader);
-    },
-    success: (data) => {
-      $('#general_modal .modal-body').html(data);
-    },
-    error: (data) => {
-      showError(data.text);
-    },
-  });
+  const beforeSend = () => {
+    $('#general_modal .modal-body').html(loader);
+  };
+  const success = (data) => {
+    $('#general_modal .modal-body').html(data);
+  };
+  ajaxGet({ url, beforeSend, success });
 });
 
 /*=============================================
 =              Add to Collection              =
 =============================================*/
-function buttonAction() {
-  const menu = $(this).next()[0];
-  const btn = this;
-  let populated = false;
-  $(menu).show().html(loader);
-
+function floatMenu(btn, menu) {
   // Positioning logic from FloatingUI, https://floating-ui.com/docs/tutorial
-  const cleanup = autoUpdate(btn, menu, () => {
+  return autoUpdate(btn, menu, () => {
     computePosition(btn, menu, {
       placement: 'bottom',
       middleware: [offset(0), flip(), shift()],
@@ -156,116 +151,113 @@ function buttonAction() {
       });
     });
   });
+}
 
-  // Hide if clicked away
-  $(document).on('click', function (event) {
-    if (!(menu.contains(event.target) || btn.contains(event.target))) {
-      if (populated) {
-        const data = $(menu).find('.collection-list').serialize();
-        $(menu).html(loader);
-        $(btn).off('click');
-        $.ajax({
-          type: 'POST',
-          url: `/collection-menu/${$(btn).attr('data-post')}`,
-          headers: {
-            'X-CSRFToken': csrftoken,
-          },
-          mode: 'same-origin',
-          data: data ? data : $.param({ collection: 'empty' }, true),
-          success: () => {
-            $(menu).hide();
-            cleanup();
-            $(btn).on('click', buttonAction);
-          },
-          error: (data) => {
-            showError(data.text);
-          },
-        });
-        populated = false;
-      }
-      $(document).off('click');
-      return;
-    }
-  });
-
-  $.ajax({
-    type: 'GET',
-    url: `/collection-menu/${$(btn).attr('data-post')}`,
-    success: (data) => {
-      let htmlOut = '<form class="collection-list"><ul class="list-group">';
-      const { response } = data;
-      for (const collection of response)
-        htmlOut += `<li class="list-group-item d-flex justify-content-between">
+function makeMenuHtml(collections) {
+  let htmlOut = '<form class="collection-list"><ul class="list-group">';
+  for (const collection of collections)
+    htmlOut += `<li class="list-group-item d-flex justify-content-between">
 <input type="checkbox" class="btn-check" id="col-${
-          collection.id
-        }" autocomplete="off" value="${
-          collection.id
-        }" aria-label="add post to ${collection.name}" ${
-          collection.checked ? 'checked' : ''
-        } name="collection">
+      collection.id
+    }" autocomplete="off" value="${collection.id}" aria-label="add post to ${
+      collection.name
+    }" ${collection.checked ? 'checked' : ''} name="collection">
 <label class="btn btn-primary w-100 text-center" for="col-${collection.id}">${
-          collection.name
-        }</label>
+      collection.name
+    }</label>
     </li>`;
-      htmlOut += `</ul></form>
+  htmlOut += `</ul></form>
   <form class="add-collection">
-    <button type="button" class="btn btn-primary w-100 mt-2" aria-label="create new Collection">
+    <button type="button" class="btn btn-primary w-100 mt-2"
+    aria-label="create new Collection">
       <i class="fas fa-plus"></i>
     </button>
-              <input class="form-control mt-2" type="text"
-              placeholder="New Collection Name" aria-label="Enter new
-              Collection name and press enter to create" required>
+          <input class="form-control mt-2" type="text"
+          placeholder="New Collection Name" aria-label="Enter new
+          Collection name and press enter to create" required>
   </form>
       `;
-      $(menu).html(htmlOut).find('.add-collection input').hide();
+  return htmlOut;
+}
+function buttonAction() {
+  const btn = this;
+  const menu = $(btn).next()[0];
+  const url = `/collection-menu/${$(btn).attr('data-post')}`;
+  let populated = false;
+  $(menu).show().html(loader);
 
-      /*----------  Add collection logic  ----------*/
-      $(menu)
-        .find('.add-collection')
-        .find('button')
-        .on('click', function () {
-          $(this).hide();
-          $('.add-collection').find('input').show().focus();
-          $('.add-collection').on('submit', function (e) {
-            e.preventDefault();
-            const name = $(this).find('input').val();
-            $.ajax({
-              type: 'POST',
-              url: '/create-collection',
-              data: { name },
-              headers: {
-                'X-CSRFToken': csrftoken,
-              },
-              success: (collection) => {
-                $(menu)
-                  .find('.collection-list')
-                  .append(
-                    `
-                  <li class="list-group-item d-flex justify-content-between">
-<input type="checkbox" class="btn-check" id="col-${collection.id}" autocomplete="off" value="${collection.id}" aria-label="add post to ${collection.name}" name="collection" checked>
-<label class="btn btn-primary w-100 text-center" for="col-${collection.id}">${collection.name}</label>
-    </li>
-`
-                  );
-                $(menu)
-                  .find('.add-collection input')
-                  .hide()
-                  .parent()
-                  .find('button')
-                  .show();
-              },
-              error: (data) => {
-                showError(data.responseJSON.text);
-              },
-            });
-          });
-        });
-      populated = true;
-    },
-    error: (data) => {
-      showError(data.text);
-    },
-  });
+  const cleanup = floatMenu(btn, menu);
+  hideAndPostOnClose();
+
+  ajaxGet({ url, success: buildMenu });
+
+  function hideAndPostOnClose() {
+    $(document).on('click', function (event) {
+      if (!(menu.contains(event.target) || btn.contains(event.target))) {
+        if (populated) {
+          const formData = $(menu).find('.collection-list').serialize();
+          const data = formData
+            ? formData
+            : $.param({ collection: 'empty' }, true);
+          $(menu).html(loader);
+          $(btn).off('click');
+          const success = () => {
+            $(menu).hide();
+            cleanup(); // stop tracking menu position
+            $(btn).on('click', buttonAction);
+          };
+          ajaxPost({ url, data, success });
+          populated = false;
+        }
+        $(document).off('click');
+      }
+    });
+  }
+  function buildMenu(data) {
+    const { response } = data;
+    const menuHtml = makeMenuHtml(response);
+    $(menu).html(menuHtml).find('.add-collection input').hide();
+    debugger;
+
+    /*----------  Add collection logic  ----------*/
+    $(menu).find('.add-collection button').on('click', addCollection);
+    populated = true;
+  }
+  function addCollection() {
+    $('.add-collection')
+      .find('button')
+      .hide()
+      .end()
+      .find('input')
+      .show()
+      .focus()
+      .end()
+      .on('submit', function (e) {
+        e.preventDefault();
+        const name = $(this).find('input').val();
+        const url = '/create-collection';
+        const success = (collection) => {
+          $(menu)
+            .find('.collection-list')
+            .append(
+              `<li class="list-group-item d-flex justify-content-between">
+<input type="checkbox" class="btn-check" id="col-${collection.id}"
+autocomplete="off" value="${collection.id}" aria-label="add post
+to ${collection.name}" name="collection" checked>
+<label class="btn btn-primary w-100 text-center"
+for="col-${collection.id}">${collection.name}</label>
+</li>`
+            );
+          $(menu)
+            .find('.add-collection input')
+            .hide()
+            .parent()
+            .find('button')
+            .show();
+        };
+        ajaxPost({ url, data: { name }, success });
+      });
+  }
 }
 
 $('button[data-action="add_to_collection"]').on('click', buttonAction);
