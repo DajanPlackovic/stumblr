4708e4a9bc5106f8d96ff8f9ff1cf109af77ea23
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-26 12:59:06 +0200

    bad styling solution for inputs via js for standard allauth forms

---
+++ static/js/allauth_laziness.js
@@ -0,0 +1,5 @@
+$('input[type="text"]').addClass('form-control');
+$('input[type="password"]').addClass('form-control');
+$('input[type="email"]').addClass('form-control');
+$('form').addClass('w-100');
+$('button').addClass('w-100');
