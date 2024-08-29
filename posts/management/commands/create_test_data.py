99c894bab18ea2a4153df18b48dd184c649dcf40
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-29 18:47:04 +0200

    add command to create follow relationships for testing purposes

--- posts/management/commands/create_test_data.py
+++ posts/management/commands/create_test_data.py
@@ -2,7 +2,7 @@ import csv
 from random import choice, sample, randint
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
-from posts.models import Post, Collection
+from posts.models import Post, Collection, Following
 
 
 class Command(BaseCommand):
@@ -13,6 +13,17 @@ class Command(BaseCommand):
             reader = csv.DictReader(file)
             for row in reader:
                 User.objects.create_user(username=row["username"])
+        users = list(User.objects.all())
+        count = 0
+        for user in users:
+            follower_num = randint(0, 10)
+            followers = sample(users, follower_num)
+            for follower in followers:
+                if follower == user:
+                    continue
+                Following.objects.create(follower=follower, followed=user)
+                count += 1
+                print(f"{count} | {follower} follows {user}")
         all_users = list(User.objects.all())
         with open('mockdata/posts.csv', 'r') as file:
             reader = csv.DictReader(file)
