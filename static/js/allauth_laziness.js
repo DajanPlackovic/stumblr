3cd06ff39b5e639e4c56072ce214d0e0ad872e6a
Dajan Plackovic <plackovicdajan@gmail.com>
2024-09-11 12:16:42 +0200

    remove unused js file

--- static/js/allauth_laziness.js
+++
@@ -1,5 +0,0 @@
-$('input[type="text"]').addClass('form-control');
-$('input[type="password"]').addClass('form-control');
-$('input[type="email"]').addClass('form-control');
-$('form').addClass('w-100');
-$('button').addClass('w-100');
